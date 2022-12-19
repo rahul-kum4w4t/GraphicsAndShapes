8d42abf56f37e9094b41dd82586f285db4506b37
Rahul <rahul.kumawat@siemens.com>
2022-12-19 22:47:47 +0530

    cell width increased as per monile device

--- main/MineSweeper/sketch.js
+++ main/MineSweeper/sketch.js
@@ -1,14 +1,14 @@
 import MineBoard from "./MineBoard.js";
 
 let mineBoard;
-let cellWidth = 40;
+let cellWidth = 80;
 let rows;
 let cols;
 
 function setup() {
-    cols = floor(windowHeight / cellWidth);
+    cols = floor(windowWidth / cellWidth);
     rows = floor(windowHeight / cellWidth);
-    createCanvas(windowHeight, windowHeight);
+    createCanvas(windowWidth, windowHeight);
     mineBoard = new MineBoard(rows, cols, cellWidth);
 }
 
